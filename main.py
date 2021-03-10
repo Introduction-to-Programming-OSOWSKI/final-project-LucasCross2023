9b0e414f5664691610f23ff9f6048132c1cadc5c
LucasCross2023 <817750@mystma.org>
2021-03-10 15:58:51 +0000

    test try 123

--- main.py
+++ main.py
@@ -27,40 +27,11 @@ def doubleWord(x):
 print(doubleWord("word"))
 
 def penguin(x):
-    if x == "emperor penguin":
-        return "near threatened"
-    if x =="king penguin":
-        return "least concern"
-    if x == "little penguin":
-        return "least concern"
-    if x == "southern rockhopper penguin":
-        return "vulnerable"
-    if x == "macaroni penguin":
-        return "vulnerable"
-    if x == "nothern rockhopper penguin":
-        return "endangered"
-    if x == "fiordland penguin":
-        return "vulnerable"
-    if x == "snares penguin":
-        return "vulnerable"
-    if x == "royal penguin":
-        return "near threatened"
-    if x == "erect-creasted penguin":
-        return "endangered"
-    if x == "yellow-eyed penguin":
-        return "endangered"
-    if x == "adelie penguin":
-        return "least concern"
-    if x == "chinstrap penguin":
-        return "least concern"
-    if x == "gentoo penguin":
-        return "least concern"
-    if x == "african penguin":
-        return "endangered"
-    if x == "humboldt penguin":
-        return "vulnerable"
+    for i in range(len(penguins)):
+        if x == penguins[i]:
+            return threat[i] 
+    
+penguins = ["king","macaroni","royal","african"]
+threat = ["increasing", "vulnerable", "near threatened", "endangered"]
 
-    if x == "galapagos penguin":
-        return "endangered"
-
-print(penguin("african penguin"))
+print(penguin("royal"))
